55c514f1c40869abd84850d929efb6dea2df47ab
Dan Johnson <danimal.johnson@gmail.com>
2020-07-29 07:23:19 -0700

    Add functionality and documentation.
    chore: Add video code.
    chore: Add additional network handling functionality.
    docs: Add readme file.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Zoom Video Conference Clone
+
+Well, maybe not an actual clone of the much more sophisticated Zoom, but still a functional example of videoconferencing using WebRTC.
+
+Based on a [tutorial](https://www.youtube.com/watch?v=DvlyzDZDEq4) by Kyle at Web Dev Simplified.
+
+## Key tech
+* WebRTC (Real Time Communications)
+* Socket.io
+* [PeerJS](https://peerjs.com)
+* EJS (Embedded JavaScript)
+* NodeJS
+* Express
+* UUID
+
+## Requirements
+* NodeJS and NPM
+* PeerJS server
+
+To use PeerJS, install it globally with `npm i -g peer` and run it in a separate terminal with `peerjs --port 3001`
--- public/script.js
+++ public/script.js
@@ -1,14 +1,67 @@
 const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
 });
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+const peers = {};
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+
+    // Display the other person's video
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    }); // on(stream)
+
+  }); // on(call)
+
+  socket.on('user-connected', userId => {
+    console.log(userId, "connected");
+    connectToNewUser(userId, stream);
+  }); // on(user-connected)
+
+}); // .then
+
+socket.on('user-disconnected', userId => {
+  console.log(userId, "disconnected");
+  if(peers[userId])
+    peers[userId].close();
+}) // on(user-disconnected)
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
-})
+}) // on(open)
 
+// socket.on('user-connected', userId => {
+//   console.log('Peer', userId, 'connected.');
+// })
 
-socket.on('user-connected', userId => {
-  console.log('User', userId, 'connected.');
-})
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  })
+  call.on('close', () => {
+    video.remove();
+  })
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  });
+  videoGrid.append(video);
+}
